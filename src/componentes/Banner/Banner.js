9d46e61a69cbc4a951813e39088b153e5f969d53
Carlos Jose Pereira <carlaopereirasanca@gmail.com>
2025-02-20 13:20:20 -0300

    estado do projeto apos aula 1 de 3

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  // JSX
+  return (
+    <header className="banner">
+      <img src="/imagens/bannerifsp.png" alt="imagem aerea" />
+    </header>
+  );
+}
+
+export default Banner;
